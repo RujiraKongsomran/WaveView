62aae46b0f95921ac484fb705e0d2a9465e9acb2
RujiraKongsomran <rujirabiwberry@gmail.com>
2020-06-24 12:47:31 +0700

    Implement WaveView

--- app/src/main/java/com/rujirakongsomran/waveview/MainActivity.java
+++ app/src/main/java/com/rujirakongsomran/waveview/MainActivity.java
@@ -2,13 +2,38 @@ package com.rujirakongsomran.waveview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
+import com.scwang.wave.MultiWaveHeader;
+
 public class MainActivity extends AppCompatActivity {
+
+    MultiWaveHeader waveHeader;
+    MultiWaveHeader waveFooter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        waveHeader = (MultiWaveHeader) findViewById(R.id.waveHeader);
+        waveFooter = (MultiWaveHeader) findViewById(R.id.waveFooter);
+
+        waveHeader.setVelocity(1);
+        waveHeader.setProgress(1);
+        waveHeader.isRunning();
+        waveHeader.setGradientAngle(45);
+        waveHeader.setWaveHeight(40);
+        waveHeader.setStartColor(Color.RED);
+        waveHeader.setCloseColor(Color.CYAN);
+
+        waveFooter.setVelocity(1);
+        waveFooter.setProgress(1);
+        waveFooter.isRunning();
+        waveFooter.setGradientAngle(45);
+        waveFooter.setWaveHeight(40);
+        waveFooter.setStartColor(Color.MAGENTA);
+        waveFooter.setCloseColor(Color.YELLOW);
     }
 }
